2e679b126831e1e9307163dc8c26a7e36f6bdd7f
Javi <javiergalandiaz@gmail.com>
2023-07-24 14:39:39 +0200

    import taxonomically curated occurrences and perform spatial and climate filtering

--- scripts/occurrences_filter.R
+++ scripts/occurrences_filter.R
@@ -1,96 +1,97 @@
 
-source('scripts/occurrences.R')
-library(terra)
+
+# CURATE OCCURRENCES SPATIALLY & CLIMATICALLY
+# THE DATABASE HAS BEEN ALREADY MANUALLY CURATED AND IT IS TAXONOMICALLY CORRECT
+
+library(tidyverse); library(readxl)
+library(terra); library(maptools); library(dismo)
 
 
-# occurrencias
-str(occ_data)
-colnames(occ_data)[colnames(occ_data)=='decimallatitude'] <- 'y'
-colnames(occ_data)[colnames(occ_data)=='decimallongitude'] <- 'x'
+occ_scleria <- read_excel("data/occ_scleria_taxcorrected.xlsx") %>% dplyr::select(-source, -id) # import occurrences
+colnames(occ_scleria) <- c('species','x','y') # correct variable names
+pts_scleria <- vect(occ_scleria, geom=c("x","y")) # create points objects
 
 
-# correct some common mistakes from widespread species
-occ_data$species[occ_data$species=='Scleria secans' & occ_data$x > 0] <- 'Scleria boivinii' # Scleria secans es Americana
-occ_data$species[occ_data$species=='Scleria hirtella' & occ_data$x > -30] <- 'Scleria distans' # Scleria hirtella es Americana
-
-# check individual ranges
-occ_data$code <- 1:nrow(occ_data)
-occ_data <- occ_data %>% filter(!(code %in% c( occ_data$code[occ_data$species=='Scleria gaertneri' & occ_data$x > 100],
-                                                occ_data$code[occ_data$species=='Scleria distans' & occ_data$x > 100],
-                                                occ_data$code[occ_data$species=='Scleria terrestris' & occ_data$x < 0],
-                                                occ_data$code[occ_data$species=='Scleria racemosa' & occ_data$x < -30],
-                                                occ_data$code[occ_data$species=='Scleria brownii' & occ_data$x > -100 & occ_data$x < 0],
-                                                occ_data$code[occ_data$species=='Scleria dregeana' & occ_data$x < 0],
-                                                occ_data$code[occ_data$species=='Scleria levis' & occ_data$x < 0],
-                                                occ_data$code[occ_data$species=='Scleria novae-hollandiae' & occ_data$x < 75],
-                                                occ_data$code[occ_data$species=='Scleria parvula' & occ_data$x < 60],
-                                                occ_data$code[occ_data$species=='Scleria pauciflora' & occ_data$y < 0],
-                                                occ_data$code[occ_data$species=='Scleria rugosa' & occ_data$x < 0]
-                                              ) ) )
-
-occ_data$code <- NULL
-occ_data$scientificname <- NULL
-occ_data$gbifid <- NULL
-head(occ_data)
+scl_countries <- read_excel("data/Kew_MNHNP_vouchers.xlsx", sheet = "botanical_countries_wcspf") # botanical countries x species
+bot_countries <- read_excel("data/occurrences/tblLevel3.xlsx") # botanical countries x political country
 
 
-# will use same variables as in Larridon et al 2021
-MAT <- rast('C:/Users/user/Desktop/wc2.1_5m_bio/wc2.1_5m_bio_1.tif') # mean annual temperature
-AP <- rast('C:/Users/user/Desktop/wc2.1_5m_bio/wc2.1_5m_bio_12.tif') # annual precipitation
-# 5 min is 9.28 km at the equator
-
-# raw plot 
-p_occ_data <- vect(occ_data, geom=c('x','y'), 'epsg:4326')
-plot(MAT, main='raw'); points(p_occ_data)
-
-# extract variables
-occ_data$cell <- terra::extract(MAT, p_occ_data, method='simple', cells=T)[,3]
-occ_data$MAT <- terra::extract(MAT, p_occ_data, method='simple')[,2]
-occ_data$AP <- terra::extract(AP, p_occ_data, method='simple')[,2]
+data(wrld_simpl) # import world map
+wrld_simpl <- vect(wrld_simpl)
+class(wrld_simpl)
 
 
-# filter
-occ_filtered <- list()
-for (sp in unique(occ_data$species)) {
+plot(wrld_simpl); points(pts_scleria, col='red')
+
+
+# correct common misidentifications of widespread species
+# plot(wrld_simpl, main='Scleria secans'); points(pts_scleria[pts_scleria$species=='Scleria secans (L.) Urb.',])
+occ_scleria$species[occ_scleria$species=='Scleria secans (L.) Urb.' & occ_scleria$x > 0] <- 'Scleria boivinii Steud.'
+# plot(wrld_simpl, main='Scleria secans'); points(pts_scleria[pts_scleria$species=='Scleria hirtella Sw.',])
+occ_scleria$species[occ_scleria$species=='Scleria hirtella Sw.' & occ_scleria$x > -30] <- 'Scleria distans Poir.'
+pts_scleria <- vect(occ_scleria, geom=c("x","y")) # redo points dataframe
+
+
+regions <- terra::extract(x=wrld_simpl, y=pts_scleria) %>% dplyr::select(FIPS, ISO2, ISO3) # extract regions
+occ_scleria <- cbind(occ_scleria, regions)
+occ_scleria <- occ_scleria %>% subset(!is.na(ISO2)) # remove points outside polygons
+
+unique(occ_scleria$species)[!(unique(occ_scleria$species) %in% scl_countries$scientific_name)]
+
+
+MAT <- rast('C:/Users/user/Desktop/wc2.1_30s_bio/wc2.1_30s_bio_1.tif') # mean annual temperature
+AP <- rast('C:/Users/user/Desktop/wc2.1_30s_bio/wc2.1_30s_bio_12.tif') # annual precipitation
+
+GRD <- MAT %>% terra::crop(pts_scleria) %>% terra::aggregate(3) %>%
+  terra::project('+proj=eqearth') # use equal area projection for spatial filter
+# plot(GRD)
+# cellSize(GRD, unit='km') # 2.15km2
+
+
+occ_scleria$MAT <- terra::extract(MAT, pts_scleria, method='simple')[,2]
+occ_scleria$AP <- terra::extract(AP, pts_scleria, method='simple')[,2]
+occ_scleria$cell <- terra::extract(GRD, terra::project(pts_scleria, '+proj=eqearth'), method='simple')[,2]
+# plot(GRD, main='MAT'); points(terra::project(pts_scleria, '+proj=eqearth'))
+
+
+occ_scleria2 <- list() # list to store results
+
+for (s in unique(occ_scleria$species)) {
   
-  # select a species
-  ss_df <- occ_data %>% filter(species == sp)
+  # filter observations of species s
+  spp_temp <- occ_scleria %>% subset(species==s)
+  # plot(wrld_simpl, main=s); points(vect(spp_temp, geom=c('x','y')), col='red')
   
-  # select one observation per cell
-  ss_df <- ss_df %>% group_by(cell) %>% slice_sample(n=1)
+  # extract botanical country information
+  count_temp <- scl_countries %>% subset(scientific_name==s) %>% dplyr::select(countries) %>%
+    deframe() %>% strsplit(" ") %>% deframe()
+  
+  # cross botanical country x iso code data
+  count_temp <- bot_countries %>% subset(L3_code %in% count_temp) %>%
+    dplyr::select(L3_ISOcode) %>% deframe() %>% unique()
+  
+  # retain observations based on iso2 code and save
+  count_temp <- spp_temp[spp_temp$ISO2 %in% count_temp,]
+  
+  # sampling bias: select one observation per 2.15km2 cell
+  ss_df <- count_temp %>% group_by(cell) %>% slice_sample(n=1)
   
   # climatic filter only for species with more than X observations
-  if (nrow(ss_df) > 20) {
-    ss_df <- ss_df %>% filter(MAT > boxplot.stats(ss_df$MAT, coef=1.5)$stats[1] & MAT < boxplot.stats(ss_df$MAT, coef=1.5)$stats[5] &
-                              AP > boxplot.stats(ss_df$AP, coef=1.5)$stats[1] & AP < boxplot.stats(ss_df$AP, coef=1.5)$stats[5] )
+  if (nrow(ss_df) > 10) {
+    ss_df <- ss_df %>% filter(MAT > boxplot.stats(ss_df$MAT, coef=2)$stats[1] & MAT < boxplot.stats(ss_df$MAT, coef=2)$stats[5] &
+                                AP > boxplot.stats(ss_df$AP, coef=2)$stats[1] & AP < boxplot.stats(ss_df$AP, coef=2)$stats[5] )
   }
   
   # save
-  occ_filtered[[sp]] <- ss_df
+  occ_scleria2[[s]] <- ss_df
+  
+  print(s)
 }
 
-# list to dataframe
-occ_filtered <- do.call(rbind, occ_filtered)
-write.table(occ_filtered, 'results/occ_filtered.txt')
+occ_scleria2 <- do.call(rbind, occ_scleria2) # paste list
+
+length(unique(occ_scleria2$species))
+unique(occ_scleria$species)[!(unique(occ_scleria$species) %in% unique(occ_scleria2$species))] # check excluded species
 
 
-# filtered plot 
-p_occ_filtered <- vect(occ_filtered, geom=c('x','y'), 'epsg:4326')
-plot(MAT, main='filtered'); points(p_occ_filtered)
-
-# check individual species
-# a.spp <- unique(occ_filtered$species[order(occ_filtered$species)])
-# for (sp in a.spp) {
-  # ss_df <- occ_filtered %>% filter(species == 'Scleria parvula')
-  # p_ss_df <- vect(ss_df, geom=c('x','y'), 'epsg:4326')
-  # plot(MAT, main=sp); points(p_ss_df)
-#   Sys.sleep(2)
-# }
-
-  
-  
-  
-  
-  
-  
-
+write.table(occ_scleria, 'results/occurrences_final.txt') # save results
