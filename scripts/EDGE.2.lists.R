c4b4e810c4a70ea2f2d648b631d1294c33744e7b
Javi <javiergalandiaz@gmail.com>
2023-09-20 12:51:23 +0200

    Function to create EDGE lists given a matrix of EDGE scores and a dataframe of IUCN categories.
    I have included an argument with ED2 values.

--- scripts/EDGE.2.lists.R
+++ scripts/EDGE.2.lists.R
@@ -12,7 +12,7 @@
 # EDGE2_scores <- EDGE2_values
 # IUCN_status <- assessments[,c('scientific_name','thr')]
 
-EDGE2_lists <- function(EDGE2_scores, IUCN_status){
+EDGE2_lists <- function(EDGE2_scores, ED2_values, IUCN_status){
 
   # check format
   if (!(is.matrix(EDGE2_scores))) { warning("EDGE2_scores not a matrix")  }
@@ -22,6 +22,8 @@ EDGE2_lists <- function(EDGE2_scores, IUCN_status){
   df_lists <- IUCN_status
   df_lists$EDGE2 <- NA
   df_lists$EDGE2_sd <- NA
+  df_lists$ED2 <- NA
+  df_lists$ED2_sd <- NA
   df_lists$perc <- NA
   df_lists$list <- NA
   
@@ -33,6 +35,9 @@ EDGE2_lists <- function(EDGE2_scores, IUCN_status){
     
     df_lists$EDGE2[s] <- median(EDGE2_scores[df_lists$scientific_name[s],], na.rm=T) # median
     df_lists$EDGE2_sd[s] <- sd(EDGE2_scores[df_lists$scientific_name[s],], na.rm=T) # sd
+
+    df_lists$ED2[s] <- median(ED2_values[df_lists$scientific_name[s],], na.rm=T) # median
+    df_lists$ED2_sd[s] <- sd(ED2_values[df_lists$scientific_name[s],], na.rm=T) # sd
     
     t1 <- table(EDGE2_scores[df_lists$scientific_name[s],] > m1) # percentile
     
@@ -46,7 +51,6 @@ EDGE2_lists <- function(EDGE2_scores, IUCN_status){
   if (df_lists$perc[s] > 0.80 & df_lists$thr[s]=='threatened') {  df_lists$list[s] <- 'borderline' }
   if (df_lists$perc[s] > 0.95 & df_lists$thr[s]=='threatened') {  df_lists$list[s] <- 'main' }
   if (df_lists$perc[s] > 0.95 & df_lists$thr[s]=='nonthreatened') {  df_lists$list[s] <- 'watch' }
-
 
   print(paste('--- ', round(s/nrow(df_lists)*100, 2), '% ---', sep='')) # status
 
